6e28675f918afb774a79ed2ad1a61f3e9a26c477
Muhammad Abdullah <abdulx125@gmail.com>
2023-10-10 12:32:47 -0700

    confused

--- script.js
+++ script.js
@@ -3,12 +3,34 @@ console.log("conn");
 let emailNode = document.getElementById("emailId");
 let passwordNode = document.getElementById("pawdId");
 let confPasswordNode = document.getElementById("confPwd");
+let errorMesssage = document.getElementsByClassName("emailErrorIcon");
 
-// now make a display error message for the email address and password make span
-function displayErrorMessage(){
+// now i will use the font awesome plugin to create the email error message when the user clicks
+
+
+function displayErrorMessage( ) {
+    errorMesssage.style.display = "inline-block";
+
+    setTimeout(function () {
+        errorMesssage.remove();
+    }, 3000);
+
 
 }
+// this function will execute the when the use clicks agin
+function again() {
+    errorMesssage.style.display = "inline";
+    // now we will put shake effect on the error message if user clicks again
+    errorMesssage.style.color = "red";
+    errorMesssage.classList.add("fa-shake");
+    setTimeout(function () {
+        errorMesssage.classList.remove("fa-shake");
+    }, 1000);
+    setTimeout(function () {
+        errorMesssage.remove();
+    }, 3000);
 
+}
 
 function formValidation(e) {
     e.preventDefault();
@@ -20,15 +42,28 @@ function formValidation(e) {
         alert("Please enter all required fields");
         return;
     } else {
-        if (emailNode.value === "") {
-            alert("Please enter email fields");
-            console.log("connecting to node ---->", emailNode.value);
+        if (emailNode.value.trim() === "") {
+            return again();
         }
-        if (passwordNode === "") {
-                    console.log("connecting to", passwordNode.value);
-                }
-        if (confPasswordNode === "") {
-                    console.log("connecting to", confPasswordNode.value);
-                }
+        if (passwordNode.value.trim() === "") {
+            return again();
+        }
+        if (confPasswordNode.value.trim() === "") {
+            return again();
+        }
     }
 }
+
+
+// emailNode.addEventListener("click", function () {
+//     if (emailNode.value === "") {
+//     displayErrorMessage();}
+// });
+
+// passwordNode.addEventListener("click", function () {
+//     displayErrorMessage();
+// });
+
+// confPasswordNode.addEventListener("click", function () {
+//     displayErrorMessage();
+// });
